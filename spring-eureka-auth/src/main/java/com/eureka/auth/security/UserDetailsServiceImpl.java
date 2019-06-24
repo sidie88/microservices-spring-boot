f4beead60eff75745586f2e4a47d43a67a53bf1c
Tarsidi <sidie88@gmail.com>
2019-06-24 12:07:29 +0700

    add docker file

--- spring-eureka-auth/src/main/java/com/eureka/auth/security/UserDetailsServiceImpl.java
+++ spring-eureka-auth/src/main/java/com/eureka/auth/security/UserDetailsServiceImpl.java
@@ -24,7 +24,7 @@ public class UserDetailsServiceImpl implements UserDetailsService  {
 		
 		// hard coding the users. All passwords must be encoded.
 		final List<AppUser> users = Arrays.asList(
-			new AppUser(1, "omar", encoder.encode("12345"), "USER"),
+			new AppUser(1, "tarsidi", encoder.encode("12345"), "USER"),
 			new AppUser(2, "admin", encoder.encode("12345"), "ADMIN")
 		);
 		
